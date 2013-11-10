4ed92ad71b0096fbbd95be6f57581ad2786e79d8
TGOlson <tydotg@gmail.com>
2013-11-10 02:44:18 -0800

    switched to koala

--- test.rb
+++ test.rb
@@ -1,24 +1,11 @@
-require 'fb_graph'
-
-# AreyouagrumpyB*tch
-# App ID:	465495653562684
-# App Secret:	a452120d11df6ee6b9dfe4b1c83540fa
+require 'koala'
 
 token = 'CAACEdEose0cBAKraDsGM8jyzZCyxR6jynD8QyqZCiLNGZBqirPFDQp0SZApqVDzJtlI07sBYpXfv80yQyjvPAgXx9iu9R9l9zU9OtfVXtRkBI1O49pZCQrXnxL3lraRT9GIA6ln3TDlvWwCCFHnIpmDZCeuU4jvPOTCOwLfyx6F9v1cMU5ZByBqJeTBfqGGZBv0ZD'
 
-user = FbGraph::User.fetch('tylerguyolson', :access_token => token)
-# p user.permissions
+@graph = Koala::Facebook::API.new(token)
 
-my_friend = user.friends.first
+p feed = @graph.get_connections("me", "feed")
+feed.each {|f| p f['message'] }
 
-id = my_friend.identifier
-token = my_friend.access_token
-
-user = FbGraph::User.fetch(id, :access_token => token)
-
-
-p user.name
-# # p user.likes
-p user.permissions
-
-# p user.posts.first
+feed = feed.next_page
+feed.each {|f| p f['message'] }
